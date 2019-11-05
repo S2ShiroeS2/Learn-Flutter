1fd622074ef2e7ea83adb5f67c822346adc969cc
S2ShiroeS2 <tuquochung123@gmail.com>
2019-11-05 15:31:22 +0700

    Tạo ra một class statefullwidget và build một list view hiển thị ra danh sách các từ tiếng anh

--- flutter_code/lib/main.dart
+++ flutter_code/lib/main.dart
@@ -27,9 +27,31 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _word = <WordPair>[]; //Mảng các từ với kiểu thể hiện của nó là WordPair
   @override
   Widget build(BuildContext context) {
-    final words = WordPair.random();
-    return Text(words.asPascalCase);
+    //final words = WordPair.random();
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        //context: Ngữ cảnh, index: vị trí của phầntử
+        //Trả về một row của listview
+        if (index >= _word.length) {
+          //Nếu giá trị index đến cuối cùng thì add thêm nhiều phần tử
+          //Mỡi lần kéo xuống sẽ add thêm 10 từ vào listview
+          _word.addAll(generateWordPairs().take(
+              10)); // dùng generateWordPairs để add vào thêm mảng 10 từ nữa.
+        }
+        return _buildRow(_word[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair word) {
+    return ListTile(
+      title: Text(
+        word.asPascalCase,
+        style: const TextStyle(fontSize: 18.0),
+      ),
+    );
   }
 }
