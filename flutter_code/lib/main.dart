ef532a1529df9ff20a2ad992ffe0d423c533081f
S2ShiroeS2 <tuquochung123@gmail.com>
2019-11-05 17:49:13 +0700

    Thêm tương tác cho ListView, dùng onTap để thay đổi màu sắc icon trong ListView

--- flutter_code/lib/main.dart
+++ flutter_code/lib/main.dart
@@ -27,7 +27,9 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _word = <WordPair>[]; //Mảng các từ với kiểu thể hiện của nó là WordPair
+  final List<WordPair> _word = <WordPair>[]; //Mảng các từ với kiểu thể hiện của nó là WordPair
+  final _setFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _listFavourite = new Set<WordPair>(); //Không cho giá trị được duplicate
   @override
   Widget build(BuildContext context) {
     //final words = WordPair.random();
@@ -35,6 +37,10 @@ class RandomWordsState extends State<RandomWords> {
       itemBuilder: (context, index) {
         //context: Ngữ cảnh, index: vị trí của phầntử
         //Trả về một row của listview
+        if (index.isOdd) {
+          //Nếu index là số lẽ
+          return Divider(); //một dấu gạch ngang
+        }
         if (index >= _word.length) {
           //Nếu giá trị index đến cuối cùng thì add thêm nhiều phần tử
           //Mỡi lần kéo xuống sẽ add thêm 10 từ vào listview
@@ -47,11 +53,27 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair word) {
+    final bool favourite_Click =
+        _listFavourite.contains(word); //Nếu list này chứa word thì đã lưu
     return ListTile(
-      title: Text(
-        word.asPascalCase,
-        style: const TextStyle(fontSize: 18.0),
-      ),
-    );
+        title: Text(
+          word.asPascalCase,
+          style: _setFont,
+        ),
+        trailing: new Icon(
+          favourite_Click == true ? Icons.favorite : Icons.favorite_border,
+          color: Colors.red,
+        ),
+        onTap: () {
+          setState(() {
+           if(favourite_Click) {
+             _listFavourite.remove(word);
+           }
+           else {
+             _listFavourite.add(word);
+           }
+          });
+        }
+      );
   }
 }
