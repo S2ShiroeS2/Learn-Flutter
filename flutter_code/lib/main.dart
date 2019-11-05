305cc76328fdd19a9d65af74c465d1119be96721
S2ShiroeS2 <tuquochung123@gmail.com>
2019-11-05 19:11:05 +0700

    Điều hướng màn hình, Chọn nhưng từ có sẵn nó sẽ được lưu vào một trang khác. -Done-

--- flutter_code/lib/main.dart
+++ flutter_code/lib/main.dart
@@ -9,14 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Welcome to Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Example for StatefulWidget"),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -27,28 +20,37 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _word = <WordPair>[]; //Mảng các từ với kiểu thể hiện của nó là WordPair
+  final List<WordPair> _word =
+      <WordPair>[]; //Mảng các từ với kiểu thể hiện của nó là WordPair
   final _setFont = const TextStyle(fontSize: 18.0);
-  final Set<WordPair> _listFavourite = new Set<WordPair>(); //Không cho giá trị được duplicate
+  final Set<WordPair> _listFavourite =
+      new Set<WordPair>(); //Không cho giá trị được duplicate
   @override
   Widget build(BuildContext context) {
-    //final words = WordPair.random();
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        //context: Ngữ cảnh, index: vị trí của phầntử
-        //Trả về một row của listview
-        if (index.isOdd) {
-          //Nếu index là số lẽ
-          return Divider(); //một dấu gạch ngang
-        }
-        if (index >= _word.length) {
-          //Nếu giá trị index đến cuối cùng thì add thêm nhiều phần tử
-          //Mỡi lần kéo xuống sẽ add thêm 10 từ vào listview
-          _word.addAll(generateWordPairs().take(
-              10)); // dùng generateWordPairs để add vào thêm mảng 10 từ nữa.
-        }
-        return _buildRow(_word[index]);
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Example for StatefulWidget"),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushItem)
+        ],
+      ),
+      body: Center(child: ListView.builder(
+        itemBuilder: (context, index) {
+          //context: Ngữ cảnh, index: vị trí của phầntử
+          //Trả về một row của listview
+          if (index.isOdd) {
+            //Nếu index là số lẽ
+            return Divider(); //một dấu gạch ngang
+          }
+          if (index >= _word.length) {
+            //Nếu giá trị index đến cuối cùng thì add thêm nhiều phần tử
+            //Mỡi lần kéo xuống sẽ add thêm 10 từ vào listview
+            _word.addAll(generateWordPairs().take(
+                10)); // dùng generateWordPairs để add vào thêm mảng 10 từ nữa.
+          }
+          return _buildRow(_word[index]);
+        },
+      )),
     );
   }
 
@@ -66,14 +68,34 @@ class RandomWordsState extends State<RandomWords> {
         ),
         onTap: () {
           setState(() {
-           if(favourite_Click) {
-             _listFavourite.remove(word);
-           }
-           else {
-             _listFavourite.add(word);
-           }
+            if (favourite_Click) {
+              _listFavourite.remove(word);
+            } else {
+              _listFavourite.add(word);
+            }
           });
-        }
-      );
+        });
   }
-}
+
+  void _pushItem() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(builder: (BuildContext context) {
+        //Đưa các item đã chọn vào danh sách và map nó sang dạng Iterable
+        final Iterable<ListTile> title = _listFavourite.map((WordPair wordPair) {
+          return new ListTile(
+            title: new Text(
+              wordPair.asPascalCase,
+              style: _setFont,
+            ),
+          );
+        });
+        final List<Widget> divided = ListTile.divideTiles(tiles: title, context: context).toList();
+        return new Scaffold(
+          appBar: new AppBar(
+            title: const Text("favourite list"),
+          ),
+          body: new ListView(children: divided),
+        );
+      }));
+    }
+  }
